ee61699ad4bf8338a90e1a3febde8526df774d0c
Noah Stacey <noahstacey1006@gmail.com>
2026-06-23 16:44:13 -0400

    Add 4-5 wire logic

--- Assets/Scripts/WIRES/WireManager.cs
+++ Assets/Scripts/WIRES/WireManager.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WireManager : MonoBehaviour
@@ -46,8 +45,8 @@ public class WireManager : MonoBehaviour
 
     void SpawnWires()
     {
-        _numberOfWires = _minNumberOfWires;
-        //_numberOfWires = Random.Range(_minNumberOfWires, _maxNumberOfWires + 1);
+        //_numberOfWires = 4;
+        _numberOfWires = Random.Range(_minNumberOfWires, _maxNumberOfWires + 1);
         _existingWires = new WireController[_numberOfWires];
         Vector3 location = new Vector3(0f, 4.2f, 0f);
         Color colour = new Color();
@@ -84,17 +83,41 @@ public class WireManager : MonoBehaviour
             else if (AreSameColour(_existingWires[0], _existingWires[2]))
                 _correctPosition = 2;
             // If there are any white wires, cut wire 1
-            else if (AreSameColour(_existingWires[0], Color.white) || AreSameColour(_existingWires[1], Color.white) || AreSameColour(_existingWires[2], Color.white))
+            else if (GroupContainsColour(_existingWires, Color.white))
                 _correctPosition = 1;
             // If any two wires are the same, cut wire 3
-            else if (AreSameColour(_existingWires[0], _existingWires[1]) || AreSameColour(_existingWires[0], _existingWires[2]) || AreSameColour(_existingWires[1], _existingWires[2]))
+            else if (AnyTwoSame(_existingWires))
                 _correctPosition = 3;
             // If there are no red wires, cut wire 2
-            else if (!AreSameColour(_existingWires[0], Color.red) && !AreSameColour(_existingWires[1], Color.red) && !AreSameColour(_existingWires[2], Color.red))
+            else if (CountColour(_existingWires, Color.red) == 0)
                 _correctPosition = 2;
             // Otherwise, cut wire 3
             else 
                 _correctPosition = 3;
+        }
+        else 
+        {
+            // If wire 1 is black, cut wire 2
+            if (AreSameColour(_existingWires[0], Color.black))
+                _correctPosition = 2;
+            // If there are more than 1 red wires, cut the last red wire
+            else if (CountColour(_existingWires, Color.red) > 1)
+                _correctPosition = LastIndexOfColour(_existingWires, Color.red) + 1;
+            // If wires 1 and 4 are the same, cut wire 4
+            else if (AreSameColour(_existingWires[0], _existingWires[3]))
+                _correctPosition = 4;
+            // If there are any yellow wires, cut wire 2
+            else if (GroupContainsColour(_existingWires, Color.yellow))
+                _correctPosition = 2;
+            // If there is exactly 1 blue wire, cut wire 1
+            else if (CountColour(_existingWires, Color.blue) == 1) 
+                _correctPosition = 1;
+            // If any two wires are the same, cut wire 3
+            else if (AnyTwoSame(_existingWires))
+                _correctPosition = 3;
+            // Otherwise, cut wire 2 
+            else 
+                _correctPosition = 2;
         }
     }
 
@@ -108,6 +131,56 @@ public class WireManager : MonoBehaviour
     bool AreSameColour(WireController wire, Color colour)
     {
         return wire.Colour == colour;
+    }
+
+    bool GroupContainsColour(WireController[] wires, Color colour)
+    {
+        foreach (WireController wire in wires)
+        {
+            if (AreSameColour(wire, colour))
+                return true;
+        }
+
+        return false;
+    }
+
+    bool AnyTwoSame(WireController[] wires)
+    {
+        for (int i = 0; i < wires.Length; i++)
+        {
+            for (int j = i + 1; j < wires.Length; j++)
+            {
+                if (AreSameColour(wires[i], wires[j]))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    int CountColour(WireController[] wires, Color colour)
+    {
+        int count = 0;
+
+        foreach (WireController wire in wires)
+        {
+            if (AreSameColour(wire, colour))
+                count++;
+        }
+
+        return count;
+    }
+
+    int LastIndexOfColour(WireController[] wires, Color colour)
+    {
+        for (int i = wires.Length - 1; i >= 0; i--)
+        {
+            if (AreSameColour(wires[i], colour))
+                return i;
+        }
+
+        // Last index not found 
+        return -1;
     }
 
 
